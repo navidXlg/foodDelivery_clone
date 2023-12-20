4d8a36ed76c4322f979c279294d99f4e2385ee56
navidXlg <navidjoker669@gmail.com>
2023-12-21 00:10:37 +0330

    Grouping card added to Homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,16 +2,18 @@ import Navabar from "../componants/Navbar";
 import Hero from "../componants/Hero";
 import pizza from "../assets/hero-image.png";
 import ProductsCatgory from "../componants/ProductsCatgory";
+import GroupingCard from "../componants/GroupingCard";
 
 
 export default function HomePage(){
 
-    return <main>
-             <section className="mx-24 mt-16 bg-gray-100  relative">
+    return <main className="mx-24">
+             <section className="mt-16 bg-gray-100 relative">
                 <Navabar/>
                 <img src={pizza} className=" z-20 relative"/>
                 <Hero/>
                 <ProductsCatgory/>
              </section>
+             <GroupingCard/>
            </main>
 };
