fc688aa6a1ad67305899222e1c3914b7c442f1cf
navidXlg <navidjoker669@gmail.com>
2023-12-20 10:38:59 +0330

    adding some pakages and navbar finishes

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,11 @@
+import Navabar from "../componants/Navbar";
+
+
+export default function HomePage(){
+
+    return <main>
+            <section className="mx-24 mt-20 bg-gray-100 p-8">
+            <Navabar/>
+            </section>
+           </main>
+};
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purpleSnapp: {
+          300: '#ff00A6',
+        },
+    },
+  },
+  plugins: [],
+}
+}
