9e904d9c0b566f61d4f4179f2d8b6a9babfc8caa
navidXlg <navidjoker669@gmail.com>
2023-12-30 17:15:41 +0330

    snappfoodBenifit section added

--- src/Pages/CustmorSubPage.js
+++ src/Pages/CustmorSubPage.js
@@ -1,4 +1,5 @@
 import NavbarSub from "../componants/NavbarSub";
+import SnappFoodBenifit from "../componants/SnappFoodBenifit";
 import SubmitionForm from "../componants/SubmitionForm";
 
 
@@ -6,9 +7,12 @@ import SubmitionForm from "../componants/SubmitionForm";
 
 export default function CustmorSubPage(){
 
-    return <div dir="rtl" className="px-56 bg-purpleSnapp-400">
+    return<> 
+           <div dir="rtl" className="px-56 pb-10 bg-purpleSnapp-400">
               <NavbarSub/>
               <SubmitionForm/>
            </div>
+           <SnappFoodBenifit/>
+           </>
 
 };
