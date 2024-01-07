a5451a4eeb8235494cb35eb716f2702e7cbb74d7
navidXlg <navidjoker669@gmail.com>
2024-01-07 10:35:29 +0330

    MainPage is responsive now

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -12,10 +12,10 @@ import Footer from "../componants/footer";
 
 export default function MainPage(){
 
-    return <div dir="rtl">
+    return <div dir="rtl" className="max-w-screen-2xl 2xl:mx-auto">
               <MainNavbar/>
-              <div className="mx-24">
-                <GroupingCard classname = "mt-72"/>
+              <div className="mx-2 sm:mx-12 md:mx-24 ">
+                <GroupingCard classname = "mt-24 sm:mt-36 md:mt-72"/>
                 <UserSuggetion/>
                 <NewProductsSnapp/>
                 <BestProducts/>
--- src/componants/MainNavbar.js
+++ src/componants/MainNavbar.js
@@ -6,6 +6,8 @@ import AuthModel from "./AuthModel";
 import { Link } from "react-router-dom";
 import { HiOutlineBuildingStorefront } from "react-icons/hi2";
 import ProductsCatgory from "./ProductsCatgory";
+import { CiSearch } from "react-icons/ci";
+import { IoPersonOutline } from "react-icons/io5";
 
 
 
@@ -14,16 +16,16 @@ export default function MainNavbar(){
     const {model, setModel} = useAuthContext();
 
 
-    return <div className="fixed w-full py-8 top-0 right-0 px-11 border-b-2 z-40 bg-white border-gray-100 shadow-sm">
-          <div className="grid grid-cols-4 justify-items-start ">
+    return <div className="fixed transform right-1/2 translate-x-1/2  w-full max-w-screen-2xl pb-8 pt-6 top-0 px-4 sm:px-11 border-b-2 z-40 bg-white border-gray-100 shadow-sm">
+          <div className="flex justify-between md:grid grid-cols-4 justify-items-start  items-center">
             <div className="flex items-center gap-4">
-                <SnappLogo className="w-16"/>
-                <div className="flex items-center">
-                    <TbCurrentLocation/>
-                    <p>برای تغییر آدرس خود کلیک کنید</p>
+                <SnappLogo className="w-16 hidden sm:block"/>
+                <div className="flex items-center gap-2">
+                    <TbCurrentLocation className="text-gray-500"/>
+                    <p className="font-semibold">برای تغییر آدرس خود کلیک کنید</p>
                 </div>
             </div>
-            <form className="col-span-2 w-4/5">   
+            <form className="col-span-2 hidden md:block w-4/5">   
             <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative">
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -34,18 +36,24 @@ export default function MainNavbar(){
                 <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="جست و جو در اسنپ فود ... " required/>
             </div>
             </form>
-             <div className="flex items-center gap-3 font-semibold text-lg justify-self-end">
+             <div className="items-center hidden md:flex gap-3 font-semibold text-lg justify-self-end">
                <Link to="/saleSubmit">
                <Button  rounded className = "flex text-sm items-center gap-2">
-                 <p>ثبت نام فروشندگان </p>
                   <HiOutlineBuildingStorefront/>
+                 <p>ثبت نام فروشندگان </p>
                   </Button>
                </Link>
                 <Button  rounded snapp className = " py-2 text-sm text-white" onClick = {() => setModel("auth")} >ورود یا عضوییت </Button>
-                {model === "auth" && <AuthModel/>}
+             </div>
+             <div className="flex items-center gap-3">
+                <CiSearch className="md:hidden"/>
+                <IoPersonOutline 
+                    className="md:hidden size-5 justify-self-end"
+                    onClick={() => setModel("auth")}/>
+                    {model === "auth" && <AuthModel/>}
              </div>
              </div>
-            <ProductsCatgory classname="mt-8"/>
+            <ProductsCatgory classname="mt-8 mx-auto w-full shadow-none"/>
         </div>  
 
     };
